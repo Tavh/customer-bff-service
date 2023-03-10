76238141635db063584d6699b216f2e4e815ba41
Tav <therzlic@cisco.com>
2023-03-10 03:00:54 +0200

    kafka config

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from kafka_producer.producer import KafkaPurchaseProducer
 from api.rest_controller import RestController
+from configparser import ConfigParser
 
+
+config = ConfigParser()
+config.read('config.ini')
+bootstrap_servers = config['kafka']['bootstrap_servers']
 
 app = Flask(__name__)
 
-producer = KafkaPurchaseProducer("localhost:9092")
+producer = KafkaPurchaseProducer(bootstrap_servers=bootstrap_servers)
 rest_controller = RestController(app=app, producer=producer)
 
 rest_controller.register_endpoints()
