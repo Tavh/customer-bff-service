ece859072910b2832c64ee71b38ced4cba5720e1
Tav <therzlic@cisco.com>
2023-03-10 17:56:08 +0200

    docs

--- api/rest_controller.py
+++ api/rest_controller.py
@@ -1,18 +1,36 @@
 from flask import Flask, jsonify
 from kafka_producer.producer import KafkaPurchaseProducer
 
-
-
 class RestController:
     def __init__(self, app: Flask, producer: KafkaPurchaseProducer):
+        """
+        Initializes a new instance of the RestController class.
+
+        Args:
+            app (Flask): The Flask application instance.
+            producer (KafkaPurchaseProducer): The KafkaPurchaseProducer instance to use for publishing purchase events.
+        """
         self.app = app
         self.producer = producer
 
         self.register_endpoints()
 
     def register_endpoints(self):
+        """
+        Registers the API endpoints for the RestController class.
+        """
         self.app.route('/customers/<int:customer_id>/purchase/<int:item_id>', methods=['POST'])(self.purchase_item)
 
     def purchase_item(self, customer_id, item_id):
+        """
+        Publishes a purchase event to Kafka.
+
+        Args:
+            customer_id (int): The ID of the customer making the purchase.
+            item_id (int): The ID of the item being purchased.
+
+        Returns:
+            A JSON response indicating that the purchase event has been published to Kafka.
+        """
         self.producer.publish_purchase(customer_id, item_id)
         return jsonify({"message": "Purchase event published to Kafka"})
--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import os
-
 from flask import Flask
 from kafka_producer.producer import KafkaPurchaseProducer
 from api.rest_controller import RestController
 from util.logger import get_logger
-
 
 # Get configuration from environment variables
 bootstrap_servers = os.environ['BOOTSTRAP_SERVERS']
@@ -12,15 +10,21 @@ topic = os.environ['TOPIC']
 
 logger = get_logger(__name__)
 
+# Log the configuration
 logger.info(f"Starting with config: bootstrap_servers={bootstrap_servers}, topic={topic}")
 
-
+# Create a Flask app
 app = Flask(__name__)
 
+# Create a KafkaPurchaseProducer instance for producing purchase events to Kafka
 producer = KafkaPurchaseProducer(bootstrap_servers=bootstrap_servers, topic=topic)
+
+# Create a RestController instance to handle incoming HTTP requests
 rest_controller = RestController(app=app, producer=producer)
 
+# Register the REST endpoints with the RestController
 rest_controller.register_endpoints()
 
 if __name__ == "__main__":
+    # Start the Flask app
     app.run(host='0.0.0.0', debug=True)
